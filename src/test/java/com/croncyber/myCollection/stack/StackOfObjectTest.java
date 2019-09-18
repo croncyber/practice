fa6713c673cd4b4fcd3626d943a1edfc206fcc28
mrBlackheart1 <lyavinets@gmail.com>
2019-09-18 09:57:05 +0300

    add test for isEmpty method

--- src/test/java/com/croncyber/myCollection/stack/StackOfObjectTest.java
+++ src/test/java/com/croncyber/myCollection/stack/StackOfObjectTest.java
@@ -1,27 +1,25 @@
 package com.croncyber.myCollection.stack;
 
 import org.junit.Test;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class StackOfObjectTest {
-    private Object[] a = {"df", "df"};
-    // stack element
-    private int N = 1;
-
-
-
-
+    private Object[] a;  // stack element
+    private int N;
     @Test
-    public void pop() {
-
-
-
+    public void isEmptyNotEmpty(){
+        StackOfObject stackOfObject = new StackOfObject(1);
+        stackOfObject.push("@");
+        assertFalse(stackOfObject.isEmpty());
     }
 
     @Test
-    public void isEmpty() {
-
+    public void isEmpty(){
+        StackOfObject stackOfObject = new StackOfObject(1);
+        assertTrue(stackOfObject.isEmpty());
     }
+
 
     @Test
     public void size() {
@@ -29,6 +27,9 @@ public class StackOfObjectTest {
 
     @Test
     public void push() {
+    }
 
+    @Test
+    public void pop() {
     }
 }
